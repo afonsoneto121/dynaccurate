c64f2e1a1b2e50bac99fd6a2c0d7120f0339574a
Afonso Neto <afonsoneto121@gmail.com>
2022-01-21 21:06:11 -0300

    Initial configuration

---
+++ src/main/java/com/br/dynaccurate/dynaccuratesolution/DynaccurateSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.br.dynaccurate.dynaccuratesolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynaccurateSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynaccurateSolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/dynaccurate/dynaccuratesolution/DynaccurateSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.dynaccurate.dynaccuratesolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynaccurateSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
